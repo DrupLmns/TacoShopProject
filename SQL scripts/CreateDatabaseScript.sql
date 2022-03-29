7995de3e6270de62709b60bd0c59d870341494b3
BeepDroid <71675347+BeepDroid@users.noreply.github.com>
2022-03-28 23:48:23 -0500

    Update CreateDatabaseScript.sql

--- SQL scripts/CreateDatabaseScript.sql
+++ SQL scripts/CreateDatabaseScript.sql
@@ -11,6 +11,14 @@ CREATE TABLE TacoShop1.dbo.Drink(
 	drink_price money
 );
 
+CREATE TABLE TacoShop1.dbo.Receipt(
+	customer_name nchar(20) null,
+	choices varchar(50) null,
+	tax money null,
+	tip money null,
+	full_price money null
+);
+
 INSERT INTO TacoShop1.dbo.Food (food_name, food_price) 
 VALUES ('Burrito', 21);
 
@@ -22,3 +30,6 @@ VALUES ('Water', 1.50);
 
 INSERT INTO TacoShop1.dbo.Drink (drink_name, drink_price) 
 VALUES ('Soda', 2.00);
+
+INSERT INTO TacoShop1.dbo.Receipt(customer_name, choices, tax, tip, full_price) VALUES ('Thomas', 'Burrito', 0.03, 1.00, 5.00);
+INSERT INTO TacoShop1.dbo.Receipt(choices) VALUES ('Taco'), ('Soda'), ('Water');
